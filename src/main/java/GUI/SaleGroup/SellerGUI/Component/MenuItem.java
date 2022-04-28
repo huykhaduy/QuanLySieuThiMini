4fcba4271a61a358a3a0f55a279c3d1997446d41
huykhaduy <huykhaduy@gmail.com>
2022-04-28 15:10:35 +0700

    Update database and java menuitem

--- src/main/java/GUI/SaleGroup/SellerGUI/Component/MenuItem.java
+++ src/main/java/GUI/SaleGroup/SellerGUI/Component/MenuItem.java
@@ -4,15 +4,12 @@
  */
 package GUI.SaleGroup.SellerGUI.Component;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-
 /**
  *
  * @author MY ACER
  */
 public class MenuItem extends RoundPanel {
-    private int maSP = 0;
+    private int maSP;
 
     public int getMaSP() {
         return maSP;
@@ -30,9 +27,13 @@ public class MenuItem extends RoundPanel {
         initComponents();
     }
     
-    public MenuItem(String string) {
+    public MenuItem(int maSP, String tenSP, String description, long giaTien, String imgPath) {
+        this.maSP = maSP;
         initComponents();
-      
+        this.productName.setText(tenSP);
+        this.lbPrice.setText(Long.toString(giaTien));
+        this.productImage.setToolTipText(description);
+        this.productImage.setImagePath(imgPath);
     }
     
        
@@ -46,10 +47,10 @@ public class MenuItem extends RoundPanel {
     private void initComponents() {
 
         imagePanel1 = new GUI.SaleGroup.SellerGUI.Component.ImagePanel();
-        imagePanel3 = new GUI.SaleGroup.SellerGUI.Component.ImagePanel();
-        jLabel1 = new javax.swing.JLabel();
+        productImage = new GUI.SaleGroup.SellerGUI.Component.ImagePanel();
+        productName = new javax.swing.JLabel();
         lbTextGiaTien = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
         lbPrice = new javax.swing.JLabel();
 
         javax.swing.GroupLayout imagePanel1Layout = new javax.swing.GroupLayout(imagePanel1);
@@ -66,32 +67,32 @@ public class MenuItem extends RoundPanel {
         setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(205, 108));
 
-        imagePanel3.setBackground(new java.awt.Color(255, 255, 255));
-        imagePanel3.setImage(new javax.swing.ImageIcon(getClass().getResource("/Assets/Image/marineboy.png"))); // NOI18N
+        productImage.setBackground(new java.awt.Color(255, 255, 255));
+        productImage.setImage(new javax.swing.ImageIcon(getClass().getResource("/Assets/Image/mitrontrungxucxich.png"))); // NOI18N
 
-        javax.swing.GroupLayout imagePanel3Layout = new javax.swing.GroupLayout(imagePanel3);
-        imagePanel3.setLayout(imagePanel3Layout);
-        imagePanel3Layout.setHorizontalGroup(
-            imagePanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout productImageLayout = new javax.swing.GroupLayout(productImage);
+        productImage.setLayout(productImageLayout);
+        productImageLayout.setHorizontalGroup(
+            productImageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 68, Short.MAX_VALUE)
         );
-        imagePanel3Layout.setVerticalGroup(
-            imagePanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        productImageLayout.setVerticalGroup(
+            productImageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 70, Short.MAX_VALUE)
         );
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(29, 186, 218));
-        jLabel1.setText("<html>Nước ngọt cocacola 500ml + ly đá");
+        productName.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        productName.setForeground(new java.awt.Color(29, 186, 218));
+        productName.setText("<html>Nước ngọt cocacola 500ml + ly đá");
 
         lbTextGiaTien.setForeground(new java.awt.Color(29, 186, 218));
         lbTextGiaTien.setText("Giá tiền:");
 
-        jButton1.setBackground(new java.awt.Color(29, 186, 218));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Thêm");
-        jButton1.setBorder(null);
+        btnAdd.setBackground(new java.awt.Color(29, 186, 218));
+        btnAdd.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btnAdd.setForeground(new java.awt.Color(255, 255, 255));
+        btnAdd.setText("Thêm");
+        btnAdd.setBorder(null);
 
         lbPrice.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         lbPrice.setForeground(new java.awt.Color(29, 186, 218));
@@ -103,42 +104,42 @@ public class MenuItem extends RoundPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(imagePanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(productImage, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lbTextGiaTien)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(lbPrice))
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(productName, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(productName, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lbTextGiaTien)
                     .addComponent(lbPrice))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
+                .addComponent(btnAdd, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
                 .addGap(12, 12, 12))
             .addGroup(layout.createSequentialGroup()
                 .addGap(18, 18, 18)
-                .addComponent(imagePanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(productImage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
     private GUI.SaleGroup.SellerGUI.Component.ImagePanel imagePanel1;
-    private GUI.SaleGroup.SellerGUI.Component.ImagePanel imagePanel3;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lbPrice;
     private javax.swing.JLabel lbTextGiaTien;
+    private GUI.SaleGroup.SellerGUI.Component.ImagePanel productImage;
+    private javax.swing.JLabel productName;
     // End of variables declaration//GEN-END:variables
 }
